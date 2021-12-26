e947ec5476020ff0e015c79102c8d3b11ad8e3f7
Lauren Tan <arr@sugarpirate.com>
2021-12-26 16:44:49 -0500

    Add jsdoc types to manhattan, prettier

--- manhattan.js
+++ manhattan.js
@@ -1,25 +1,31 @@
-import createLogger from "./create-logger.js";
-
 /**
- * Recursively traverses reachable servers beginning from the current host, opening ports as
- * needed and nuking if possible. If the server was successfully nuked, we copy over the
- * AGENT_SCRIPT which makes use of the nuked server to hack the most efficient server.
+ * @typedef { import('./bitburner.d').NS } NS
+ * @typedef { import('./bitburner.d').Server } Server
+ *
+ * @typedef {AGENT_GROW_SCRIPT | AGENT_HACK_SCRIPT | AGENT_WEAK_SCRIPT} AgentScript
  */
 
-const ROOT_NODE = "home";
-const FLEET_PREFIX = "fleet-node";
+import createLogger from './create-logger.js';
+
+const ROOT_NODE = 'home';
+const FLEET_PREFIX = 'fleet-node';
 const DISPATCH_INTERVAL = 500;
 const LOOP_INTERVAL = 1_000;
 const WEAK_AMOUNT = 0.05;
-export const AGENT_GROW_SCRIPT = "agent-grow.js";
-export const AGENT_HACK_SCRIPT = "agent-hack.js";
-export const AGENT_WEAK_SCRIPT = "agent-weak.js";
+export const AGENT_GROW_SCRIPT = 'agent-grow.js';
+export const AGENT_HACK_SCRIPT = 'agent-hack.js';
+export const AGENT_WEAK_SCRIPT = 'agent-weak.js';
 const AGENT_PAYLOAD = new Set([
   AGENT_GROW_SCRIPT,
   AGENT_HACK_SCRIPT,
   AGENT_WEAK_SCRIPT,
 ]);
 
+/**
+ * @param {number} start
+ * @param {number} end
+ * @returns {Generator<number>}
+ */
 function* makeCycle(start, end) {
   let ii = start;
   while (true) {
@@ -33,40 +39,60 @@ function* makeCycle(start, end) {
 /** @param {NS} ns **/
 export async function main(ns) {
   ns.tail();
-  ns.disableLog("disableLog");
-  ns.disableLog("getServerNumPortsRequired");
-  ns.disableLog("scan");
-  ns.disableLog("getServerUsedRam");
-  ns.disableLog("getServerMaxRam");
-  ns.disableLog("kill");
-  ns.disableLog("killall");
-  ns.disableLog("scp");
-  ns.disableLog("exec");
-  ns.disableLog("rm");
-  ns.disableLog("getServerSecurityLevel");
-  ns.disableLog("getServerMinSecurityLevel");
-  ns.disableLog("getServerMoneyAvailable");
-  ns.disableLog("getServerMaxMoney");
-  ns.disableLog("sleep");
-  ns.disableLog("nuke");
-  ns.disableLog("brutessh");
-  ns.disableLog("ftpcrack");
-  ns.disableLog("relaysmtp");
-  ns.disableLog("httpworm");
-  ns.disableLog("sqlinject");
+  ns.disableLog('disableLog');
+  ns.disableLog('getServerNumPortsRequired');
+  ns.disableLog('scan');
+  ns.disableLog('getServerUsedRam');
+  ns.disableLog('getServerMaxRam');
+  ns.disableLog('kill');
+  ns.disableLog('killall');
+  ns.disableLog('scp');
+  ns.disableLog('exec');
+  ns.disableLog('rm');
+  ns.disableLog('getServerSecurityLevel');
+  ns.disableLog('getServerMinSecurityLevel');
+  ns.disableLog('getServerMoneyAvailable');
+  ns.disableLog('getServerMaxMoney');
+  ns.disableLog('sleep');
+  ns.disableLog('nuke');
+  ns.disableLog('brutessh');
+  ns.disableLog('ftpcrack');
+  ns.disableLog('relaysmtp');
+  ns.disableLog('httpworm');
+  ns.disableLog('sqlinject');
 
   const log = createLogger(ns);
   const currentHost = ns.getHostname();
 
-  const formatThreads = (x) => ns.nFormat(x, "0,0.00a");
-  const formatMoney = (x) => ns.nFormat(x, "($0.00a)");
-  const format2Decimals = (x) => ns.nFormat(x, "0,0.00");
-  const formatPercent = (x) => ns.nFormat(x, "000.0%");
+  /** @param {number} n */
+  const formatThreads = (n) => ns.nFormat(n, '0,0.00a');
+  /** @param {number} n */
+  const formatMoney = (n) => ns.nFormat(n, '($0.00a)');
+  /** @param {number} n */
+  const format2Decimals = (n) => ns.nFormat(n, '0,0.00');
+  /** @param {number} n */
+  const formatPercent = (n) => ns.nFormat(n, '000.0%');
 
+  /**
+   * @param {string} hostname
+   * @returns {boolean}
+   */
   const isHome = (hostname) => hostname === ROOT_NODE;
+  /**
+   * @param {string} hostname
+   * @returns {boolean}
+   */
   const isFleet = (hostname) => hostname.startsWith(FLEET_PREFIX);
+  /**
+   * @param {string} hostname
+   * @returns {boolean}
+   */
   const isOwned = (hostname) => isHome(hostname) || isFleet(hostname);
 
+  /**
+   * @param {string} hostname
+   * @returns {number}
+   */
   const getGrowThreads = (hostname) => {
     const moneyAvail = ns.getServerMoneyAvailable(hostname);
     const moneyMax = ns.getServerMaxMoney(hostname);
@@ -79,6 +105,10 @@ export async function main(ns) {
     }
     return Math.ceil(ns.growthAnalyze(hostname, growRate) * 10000);
   };
+  /**
+   * @param {string} hostname
+   * @returns {number}
+   */
   const getHackThreads = (hostname) => {
     const moneyAvail = ns.getServerMoneyAvailable(hostname);
     const moneyMax = ns.getServerMaxMoney(hostname);
@@ -89,6 +119,10 @@ export async function main(ns) {
       (moneyMax / (moneyAvail * ns.hackAnalyze(hostname))) * 10000
     );
   };
+  /**
+   * @param {string} hostname
+   * @returns {number}
+   */
   const getWeakThreads = (hostname) =>
     Math.ceil(
       (ns.getServerSecurityLevel(hostname) -
@@ -96,13 +130,28 @@ export async function main(ns) {
         WEAK_AMOUNT
     );
 
+  /**
+   * @param {Server} destination
+   * @returns {number}
+   */
   const getGrowTime = (destination) =>
     Math.ceil(ns.getGrowTime(destination.hostname));
+  /**
+   * @param {Server} destination
+   * @returns {number}
+   */
   const getWeakTime = (destination) =>
     Math.ceil(ns.getWeakenTime(destination.hostname));
+  /**
+   * @param {Server} destination
+   * @returns {number}
+   */
   const getHackTime = (destination) =>
     Math.ceil(ns.getHackTime(destination.hostname));
 
+  /**
+   * @param {Server} destination
+   */
   const report = (destination) => {
     const moneyCurr = ns.getServerMoneyAvailable(destination.hostname);
     const moneyMax = ns.getServerMaxMoney(destination.hostname);
@@ -121,6 +170,10 @@ export async function main(ns) {
     );
   };
 
+  /**
+   * @param {string} hostname
+   * @returns {boolean}
+   */
   const tryNuke = (hostname) => {
     if (isOwned(hostname)) {
       return false;
@@ -132,28 +185,28 @@ export async function main(ns) {
     if (user.hacking < server.requiredHackingSkill) {
       log(
         `Expected hacking level ${server.requiredHackingSkill} for ${server.hostname}, got: ${user.hacking}`,
-        "warning"
+        'warning'
       );
       return false;
     }
 
-    if (server.sshPortOpen === false && ns.fileExists("BruteSSH.exe")) {
+    if (server.sshPortOpen === false && ns.fileExists('BruteSSH.exe')) {
       ns.brutessh(server.hostname);
     }
 
-    if (server.ftpPortOpen === false && ns.fileExists("FTPCrack.exe")) {
+    if (server.ftpPortOpen === false && ns.fileExists('FTPCrack.exe')) {
       ns.ftpcrack(server.hostname);
     }
 
-    if (server.smtpPortOpen === false && ns.fileExists("relaySMTP.exe")) {
+    if (server.smtpPortOpen === false && ns.fileExists('relaySMTP.exe')) {
       ns.relaysmtp(server.hostname);
     }
 
-    if (server.httpPortOpen === false && ns.fileExists("HTTPWorm.exe")) {
+    if (server.httpPortOpen === false && ns.fileExists('HTTPWorm.exe')) {
       ns.httpworm(server.hostname);
     }
 
-    if (server.sqlPortOpen === false && ns.fileExists("SQLInject.exe")) {
+    if (server.sqlPortOpen === false && ns.fileExists('SQLInject.exe')) {
       ns.sqlinject(server.hostname);
     }
 
@@ -167,6 +220,9 @@ export async function main(ns) {
 
     return ns.hasRootAccess(server.hostname);
   };
+  /**
+   * @param {Set<string>} nukedHostnames
+   */
   const installAgents = async (nukedHostnames) => {
     for (const server of getControlledServers(nukedHostnames)) {
       if (isHome(server.hostname)) {
@@ -175,10 +231,15 @@ export async function main(ns) {
       ns.killall(server.hostname);
       for (const script of AGENT_PAYLOAD) {
         ns.rm(script, server.hostname);
-        await ns.scp(script, server.hostname);
+        await ns.scp(script, currentHost, server.hostname);
       }
     }
   };
+  /**
+   * @param {Set<string>} nukedHostnames
+   * @param {Server} destination
+   * @param {AgentScript} script
+   */
   const killScriptOnAllServers = (nukedHostnames, destination, script) => {
     for (const server of getControlledServers(nukedHostnames)) {
       for (const process of ns.ps(server.hostname)) {
@@ -192,6 +253,11 @@ export async function main(ns) {
   const createTraversal = () => {
     const visited = new Set();
     const nukedHostnames = new Set();
+    /**
+     * @param {string} hostname
+     * @param {number} depth
+     * @returns {Set<string>}
+     */
     return function traverse(hostname, depth = 0) {
       for (const nextHostname of ns.scan(hostname)) {
         if (isOwned(nextHostname) || visited.has(nextHostname)) {
@@ -206,12 +272,20 @@ export async function main(ns) {
       return nukedHostnames;
     };
   };
+  /**
+   * @param {Set<string>} nukedHostnames
+   * @returns {Server[]}
+   */
   const getControlledServers = (nukedHostnames) =>
     [ROOT_NODE, ...nukedHostnames, ...ns.getPurchasedServers()]
       .map((hostname) => ns.getServer(hostname))
       .sort((a, b) => b.cpuCores * b.maxRam - a.cpuCores * a.maxRam);
+  /**
+   * @param {Set<string>} nukedHostnames
+   * @returns {Server[]}
+   */
   const getRankedDestinations = (nukedHostnames) => {
-    const rankedDestinations = new Array(nukedHostnames.length);
+    const rankedDestinations = [];
     for (const hostname of nukedHostnames) {
       const server = ns.getServer(hostname);
       if (server.moneyMax === 0) {
@@ -222,6 +296,13 @@ export async function main(ns) {
     return rankedDestinations.sort((a, b) => b.moneyMax - a.moneyMax);
   };
 
+  /**
+   * @param {Server} source
+   * @param {Server} destination
+   * @param {AgentScript} script
+   * @param {{threadsNeeded: number, delay: number}} options
+   * @returns {{threadsSpawned: number, threadsRemaining: number, ramUsed: number} | null}
+   */
   const execScript = (
     source,
     destination,
@@ -240,7 +321,7 @@ export async function main(ns) {
       threadsAvail > threadsNeeded ? threadsNeeded : threadsAvail,
       1
     );
-    const scriptArgs = [destination.hostname, delay];
+    const scriptArgs = [destination.hostname, delay.toString()];
     if (ns.exec(script, source.hostname, threads, ...scriptArgs) !== 0) {
       return {
         threadsSpawned: threads,
@@ -250,6 +331,11 @@ export async function main(ns) {
     }
     return null;
   };
+  /**
+   * @param {Set<string>} nukedHostnames
+   * @param {Server} destination
+   * @returns {Promise<number>}
+   */
   const dispatchWeak = async (nukedHostnames, destination) => {
     let weakensRemaining = getWeakThreads(destination.hostname);
     let longestTimeTaken = -Infinity;
@@ -283,6 +369,11 @@ export async function main(ns) {
     }
     return longestTimeTaken;
   };
+  /**
+   * @param {Set<string>} nukedHostnames
+   * @param {Server} destination
+   * @returns {Promise<number>}
+   */
   const dispatchGrow = async (nukedHostnames, destination) => {
     let growsRemaining = getGrowThreads(destination.hostname);
     let weakensRemaining = Math.ceil(
@@ -329,6 +420,11 @@ export async function main(ns) {
     }
     return longestTimeTaken;
   };
+  /**
+   * @param {Set<string>} nukedHostnames
+   * @param {Server} destination
+   * @returns {Promise<number>}
+   */
   const dispatchHack = async (nukedHostnames, destination) => {
     let hacksRemaining = getHackThreads(destination.hostname);
     let weakensRemaining = Math.ceil(
@@ -376,14 +472,17 @@ export async function main(ns) {
     return longestTimeTaken;
   };
 
-  // Note: This is an infinite loop cycling through servers
+  /**
+   * Note: This is an infinite loop cycling through servers
+   * @param {Set<string>} nukedHostnames
+   */
   const orchestrateControlledServers = async (nukedHostnames) => {
     const rankedDestinations = getRankedDestinations(nukedHostnames);
     for (const destinationIdx of makeCycle(0, rankedDestinations.length - 1)) {
       const traverse = createTraversal();
       const newTraversedHostnames = traverse(currentHost);
-      if (newTraversedHostnames.length !== nukedHostnames.length) {
-        log(`New nukable servers detected`, "warning");
+      if (newTraversedHostnames.size !== nukedHostnames.size) {
+        log(`New nukable servers detected`, 'warning');
         return;
       }
       const destination = rankedDestinations[destinationIdx];
