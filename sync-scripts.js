6623754a4d80f9856fbbd55e30dbb09af5e8ae26
Lauren Tan <arr@sugarpirate.com>
2021-12-27 17:14:46 -0500

    Pull from correct branch

--- sync-scripts.js
+++ sync-scripts.js
@@ -2,8 +2,6 @@
 
 import createLogger from './create-logger.js';
 
-const REPO_URL =
-  'https://api.github.com/repos/poteto/bitburner-scripts/git/trees/main?recursive=1';
 const REPO_TXT = 'repo.json.txt';
 
 /** @param {NS} ns **/
@@ -15,18 +13,18 @@ export async function main(ns) {
   const log = createLogger(ns);
   const { branch } = ns.flags([['branch', 'main']]);
 
-  if ((await ns.wget(REPO_URL, 'repo.json.txt')) === false) {
+  const repoUrl = `https://api.github.com/repos/poteto/bitburner-scripts/git/trees/${branch}?recursive=1`;
+  if ((await ns.wget(repoUrl, 'repo.json.txt')) === false) {
     log(`Couldn't get repo manifest`, 'error');
     ns.exit();
   }
-
   const repo = JSON.parse(ns.read(REPO_TXT));
   for (const file of repo.tree) {
     if (file.type !== 'blob' || file.path.endsWith('.js') === false) {
       continue;
     }
-    const url = `https://raw.githubusercontent.com/poteto/bitburner-scripts/${branch}/${file.path}`;
-    if (await ns.wget(url, file.path)) {
+    const fileUrl = `https://raw.githubusercontent.com/poteto/bitburner-scripts/${branch}/${file.path}`;
+    if (await ns.wget(fileUrl, file.path)) {
       log(`Successfully synced ${file.path} (${file.sha})`);
     } else {
       log(`Couldn't get ${file.path} (${file.sha})`);
