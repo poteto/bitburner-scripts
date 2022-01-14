46c17141ede23e002a1c243fe03352bd9fa885a5
Lauren Tan <arr@sugarpirate.com>
2022-01-13 23:01:26 -0500

    Get money from hacknet

---
+++ hacknet-money.js
@@ -0,0 +1,15 @@
+/** @typedef { import('./bitburner.d').NS } NS */
+
+const INTERVAL = 1_000;
+
+/** @param {NS} ns **/
+export async function main(ns) {
+  ns.tail();
+  ns.disableLog('disableLog');
+  ns.disableLog('sleep');
+
+  while (true) {
+    ns.hacknet.spendHashes('Sell for Money');
+    await ns.sleep(INTERVAL);
+  }
+}
