85aa3c6fee244fa96a3a91dee43aeffff0d435a5
Lauren Tan <arr@sugarpirate.com>
2022-01-19 00:37:03 -0500

    Clean up agent-share

--- agent-share.js
+++ agent-share.js
@@ -2,10 +2,7 @@
 
 /** @param {NS} ns **/
 export async function main(ns) {
-  const [targetHostname, instanceId] = ns.args;
-  ns.print(
-    `[INFO] Started instance ${instanceId} at: ${new Date().toLocaleString()}`
-  );
+  ns.print(`[INFO] Started at: ${new Date().toLocaleString()}`);
   while (true) {
     await ns.share();
   }
